f843f97e1eb607a5e0e9efc109a1e533ac073f48
Danijel B <danijel.balaz1@gmail.com>
2022-06-17 23:03:23 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
 function computerPlay() {
-    const rockPaperScissorsPick = ['Rock', 'Paper', 'Scissors'];
+    const rockPaperScissorsPick = ['rock', 'paper', 'scissors'];
     const valueToUse = rockPaperScissorsPick[Math.floor(Math.random() * rockPaperScissorsPick.length)]
-    console.log(valueToUse)
+    return valueToUse
 }
 
-computerPlay()
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+
+    if (playerSelection === computerSelection) {
+        console.log('Oh no! A tie');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('You Win! Rock beats Scissors');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You Win! Scissors beat Paper');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You Win! Paper beats Rock');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('You Lose! Rock beats Scissors');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('You Lose! Scissors beats Paper');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('You Lose! Paper beats Rock');
+    }
+}
+
+const playerSelection = 'scissors';
+console.log(playerSelection.toLowerCase())
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection))
