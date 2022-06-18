6cadcf4e750154fd31c1577e884a5955e57e4ea8
Danijel B <danijel.balaz1@gmail.com>
2022-06-18 16:18:55 +0200

    Add result to playRound function

--- javascript.js
+++ javascript.js
@@ -8,20 +8,21 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
 
     if (playerSelection === computerSelection) {
-        console.log('Oh no! A tie');
+        result = 'Oh no! A tie';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('You Win! Rock beats Scissors');
+        result = 'You Win! Rock beats Scissors';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('You Win! Scissors beat Paper');
+        result = 'You Win! Scissors beat Paper';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('You Win! Paper beats Rock');
+        result = 'You Win! Paper beats Rock';
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You Lose! Rock beats Scissors');
+        result = 'You Lose! Rock beats Scissors';
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You Lose! Scissors beats Paper');
+        result = 'You Lose! Scissors beats Paper';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You Lose! Paper beats Rock');
+        result = 'You Lose! Paper beats Rock';
     }
+    return result
 }
 
 const playerSelection = 'scissors';
